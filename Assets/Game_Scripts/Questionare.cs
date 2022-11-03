307d45ae5d49a3c2e3fc72e82eace6115e65b832
Katherine Neul (neulk) <Katherine.neul@hotmail.com>
2022-11-03 02:47:20 +0100

    More Tests and form
    *truck and car sounds
    *2 or 3 new scenes
    *questionnaire added, works with google forms.

---
+++ Assets/Game_Scripts/Questionare.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class Questionare : MonoBehaviour
+{
+    [SerializeField] TMP_InputField feedback, name;
+    [SerializeField] TMP_Dropdown dropdown;
+
+    string URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSebfuxtCDgYzhjkuXqwbgtEvYkJfZY4_cafOLdUYdwD3DQ28Q/formResponse";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Send()
+    {
+        StartCoroutine(Post(feedback.text, name.text, dropdown.value));
+    }
+
+    IEnumerator Post(string s1, string n, int d1)
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("entry.2017985885", s1);
+        form.AddField("entry.1064095882", n);
+        form.AddField("entry.936826908", d1);
+        
+        DropDown();
+
+        byte[] ramData = form.data;
+        UnityWebRequest www = UnityWebRequest.Post(URL, form);
+
+        yield return www.SendWebRequest();
+    }
+
+    void DropDown()
+    {
+        if (dropdown.value == 0)
+        {
+            Debug.Log(dropdown.value);
+            //dropdown.value == "Yes";
+        }
+        else if (dropdown.value == 1)
+        {
+            Debug.Log(dropdown.value);
+        }
+        else if (dropdown.value == 2)
+        {
+            Debug.Log(dropdown.value);
+        }
+        else { /*do nothing*/ }
+    }
+}
---
+++ Assets/Game_Scripts/Sound_Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound_Player : MonoBehaviour
+{
+    AudioSource audioSource; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
